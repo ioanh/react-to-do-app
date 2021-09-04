62f3374383868fe78697c93dd9915f7fd9f5c83c
ioanh <ioan.xristov@abv.bg>
2021-09-04 18:53:32 +0300

    playint with react while on work xD

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
 export default class ToDo extends Component {
+    uncheck(id){
+        console.log(id)
+    }
     render() {
         return (
             <div>
                 {this.props.todo.name}
                 <input type="checkbox" checked={this.props.todo.complete}></input>
+                <button onClick={uncheck(this.props.todo.id)}>Console log the id of the current element</button>
             </div>
         )
     }
