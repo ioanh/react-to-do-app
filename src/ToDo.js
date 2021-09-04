e608f66f17e5c7fe4abe4a3b27c5306a7959b741
ioanh <ioan.xristov@abv.bg>
2021-09-05 01:39:03 +0300

    finished it

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
 export default class ToDo extends Component {
-    onToggle(){
-        console.log('wtf')
+    onToggle = () => {
+        this.props.toggleToDo(this.props.todo.id)
     }
     render() {
         return (
             <div>
                 {this.props.todo.name}
-                <input onClick={this.onToggle} type="checkbox" Checked={this.props.todo.complete} ></input>
+                <input onChange={this.onToggle}  type="checkbox" checked={this.props.todo.complete} ></input>
             </div>
         )
     }
